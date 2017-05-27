28c9c763349900cda138a23ef7c370445013b530
Aaron Parker <aaron@stealthpuppy.com>
2017-05-27 12:08:38 -0400

    Update Pester test file

--- Install-VisualCRedistributables.Tests.ps1
+++ Install-VisualCRedistributables.Tests.ps1
@@ -1,7 +1,7 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\bin\$sut" -Xml .\bin\VisualCRedistributablesSupported.xml -WhatIf
- 
+
 Describe "Install-VisualCRedistributables" {
     It 'Downloads the Visual C++ Redistributable' {
         Mock -CommandName 'Invoke-WebRequest' -MockWith {
