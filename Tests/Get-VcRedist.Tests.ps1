928ba4c056c089f139eed238c6bee1a58b0ab969
Aaron Parker <aaron@stealthpuppy.com>
2018-03-19 15:43:29 +1100

    Added Get-VcRedist.Tests.ps1

---
+++ Tests/Get-VcRedist.Tests.ps1
@@ -0,0 +1,41 @@
+
+Function Test-VcDownloads {
+    <#
+        .SYNOPSIS
+            Tests downloads from Get-VcList are sucessful.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    Param (
+        [Parameter()]
+        [array]$VcList,
+
+        [Parameter()]
+        [string]$Path
+    )
+
+    $Output = $False
+    ForEach ($Vc in $VcList) {
+        $Target = "$($(Get-Item -Path $Path).FullName)\$($Vc.Release)\$($Vc.Architecture)\$($Vc.ShortName)"
+        If (Test-Path -Path "$Target\$(Split-Path -Path $Vc.Download -Leaf)" -PathType Leaf) {
+            Write-Verbose "$Target\$(Split-Path -Path $Vc.Download -Leaf) - exists."
+            $Output = $True
+        } Else {
+            Write-Warning "$Target\$(Split-Path -Path $Vc.Download -Leaf) - not found."
+            $Output = $False
+        }
+    }
+    Write-Output $Output
+}
+
+# Pester tests
+Describe 'Get-VcRedist' {
+    Context "Download Redistributables" {
+        It "Downloads supported Visual C++ Redistributables" {
+            $Path = ".\VcDownload"
+            If (!(Test-Path $Path)) { New-Item $Path -ItemType Directory -Force -Verbose }
+            $VcList = Get-VcList -Verbose
+            $Downloads = Get-VcRedist -VcList $VcList -Path $Path -Verbose
+            Test-VcDownloads -VcList $Downloads -Path $Path | Should -Be $True
+        }
+    }
+}
--- Tests/Get-VcXml.Tests.ps1
+++ Tests/Get-VcXml.Tests.ps1
@@ -2,11 +2,11 @@
 Describe 'Get-VcList' {
     Context "Return built-in manifest" {
         It "Given no parameters, it returns supported Visual C++ Redistributables" {
-            $VcList = Get-VcList
+            $VcList = Get-VcList -Verbose
             $VcList.Count | Should -Be 12
         }
         It "Given valid parameter -Export 'All', it returns all Visual C++ Redistributables" {
-            $VcList = Get-VcList -Export All
+            $VcList = Get-VcList -Export All -Verbose
             $VcList.Count | Should -Be 32
         }
     }
@@ -14,7 +14,7 @@ Describe 'Get-VcList' {
         It "Given valid parameter -Xml, it returns Visual C++ Redistributables from an external manifest" {
             $Xml = "$($pwd)\Redists.xml"
             Export-VcXml -Path $Xml -Verbose
-            $VcList = Get-VcList -Xml $Xml
+            $VcList = Get-VcList -Xml $Xml -Verbose
             $VcList.Count | Should -Be 12
         }
     }
