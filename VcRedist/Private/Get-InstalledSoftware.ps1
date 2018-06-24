bd74f76ad72c1a1e2b30d259b6c55f2270432e70
Aaron Parker <aaron@stealthpuppy.com>
2018-06-24 18:40:03 +1000

    Added Get-InstalledSoftware.ps1; Add Version to Get-VcList

---
+++ VcRedist/Private/Get-InstalledSoftware.ps1
@@ -0,0 +1,51 @@
+Function Get-InstalledSoftware {
+    <#
+        .SYNOPSIS
+            Retrieves a list of all software installed
+
+        .EXAMPLE
+            Get-InstalledSoftware
+            
+            This example retrieves all software installed on the local computer
+            
+        .PARAMETER Name
+            The software title you'd like to limit the query to.
+
+        .NOTES
+            Author: Adam Bertram
+            URL: https://4sysops.com/archives/find-the-product-guid-of-installed-software-with-powershell/
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name
+    )
+
+    $UninstallKeys = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall", "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+    $null = New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS
+    $UninstallKeys += Get-ChildItem HKU: -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | ForEach-Object { "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall" }
+    if (-not $UninstallKeys) {
+        Write-Verbose -Message 'No software registry keys found'
+    }
+    else {
+        foreach ($UninstallKey in $UninstallKeys) {
+            if ($PSBoundParameters.ContainsKey('Name')) {
+                $WhereBlock = { ($_.PSChildName -match '^{[A-Z0-9]{8}-([A-Z0-9]{4}-){3}[A-Z0-9]{12}}$') -and ($_.GetValue('DisplayName') -like "$Name*") }
+            }
+            else {
+                $WhereBlock = { ($_.PSChildName -match '^{[A-Z0-9]{8}-([A-Z0-9]{4}-){3}[A-Z0-9]{12}}$') -and ($_.GetValue('DisplayName')) }
+            }
+            $gciParams = @{
+                Path        = $UninstallKey
+                ErrorAction = 'SilentlyContinue'
+            }
+            $selectProperties = @(
+                @{n = 'GUID'; e = {$_.PSChildName}}, 
+                @{n = 'Name'; e = {$_.GetValue('DisplayName')}}
+            )
+            Get-ChildItem @gciParams | Where-Object $WhereBlock | Select-Object -Property $selectProperties
+        }
+    }
+}
