54e5f12436b9c81eb1c52ce205b9433f3c510d44
Aaron Parker <aaron@stealthpuppy.com>
2019-04-03 19:30:07 +1100

    Update Get-InstalledSoftware to get year correctly

--- scripts/Uninstall-VcRedist.ps1
+++ scripts/Uninstall-VcRedist.ps1
@@ -1,9 +1,19 @@
 <#
-    Uninstall Visual C++ Redistributables
+    .SYNOPSIS    
+        Uninstall all of the installed Visual C++ Redistributables on the local system
 #>
 
+[CmdletBinding()]
+param (
+)
+
 $InstalledVcRedists = Get-InstalledVcRedist
-ForEach ($VcRedist in $InstalledVcRedists) {
-    Write-Host $VcRedist.Name
-    Start-Process -FilePath "$env:SystemRoot\System32\cmd.exe" -ArgumentList "/c $($VcRedist.UninstallString) /passive" -Wait
+ForEach ($Vc in $InstalledVcRedists) {
+    Write-Output $Vc.Name
+    If ($Null -eq $vc.QuietUninstallString) {
+        Start-Process -FilePath "$env:SystemRoot\System32\cmd.exe" -ArgumentList $vc.QuietUninstallString -Wait
+    }
+    Else {
+        Start-Process -FilePath "$env:SystemRoot\System32\cmd.exe" -ArgumentList "/c $($Vc.UninstallString) /passive" -Wait
+    }
 }
