5ec4da8785f65d42fbd6bf479f8d90f2606bb37c
Aaron Parker <aaron@stealthpuppy.com>
2021-12-19 16:40:26 +1100

    Update Tests.ps1

--- ci/Tests.ps1
+++ ci/Tests.ps1
@@ -2,9 +2,9 @@
     .SYNOPSIS
         AppVeyor tests script.
 #>
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUserDeclaredVarsMoreThanAssignments", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUserDeclaredVarsMoreThanAssignments")]
 [OutputType()]
 Param ()
 
@@ -13,7 +13,7 @@ If (Test-Path -Path "env:APPVEYOR_BUILD_FOLDER") {
     Import-Module (Join-Path -Path $ProjectRoot -ChildPath "VcRedist") -Force
 
     # Invoke Pester tests and upload results to AppVeyor
-    $res = Invoke-Pester -Path $tests -OutputFormat NUnitXml -OutputFile $output -PassThru
+    $res = Invoke-Pester -Path $tests -OutputFormat "NUnitXml" -OutputFile $output -PassThru
     If ($res.FailedCount -gt 0) { Throw "$($res.FailedCount) tests failed." }
     If (Test-Path -Path env:APPVEYOR_JOB_ID) {
         (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path -Path $output))
