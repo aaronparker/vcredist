d0011f0a843f74227a6c79f3df7de585ade11455
Aaron Parker <aaron@stealthpuppy.com>
2023-01-10 10:10:18 +1100

    Add push tag script

---
+++ ci/Push-Tag.ps1
@@ -0,0 +1,13 @@
+<#
+    .SYNOPSIS
+        Set a tag and push
+#>
+[CmdletBinding()]
+param()
+
+$Path = Resolve-Path -Path (((Get-Item (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)).Parent).FullName)
+$Module = Test-ModuleManifest -Path "$Path/VcRedist/VcRedist.psd1"
+if ($null -ne $Module) {
+    git tag "v$($Module.Version.ToString())"
+    git push origin --tags
+}
