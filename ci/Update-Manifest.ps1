db7ea61b8ddef83e7611daf50f51fed1091ae4b6
Aaron Parker <aaron@stealthpuppy.com>
2025-06-01 13:38:13 +1000

    Update manifest support for ARM64

--- ci/Update-Manifest.ps1
+++ ci/Update-Manifest.ps1
@@ -8,6 +8,7 @@
 [CmdletBinding()]
 param (
     [System.String[]] $Release,
+    [System.String[]] $Architecture = @("x64", "x86"),
     [System.String] $Path,
     [System.String] $VcManifest
 )
@@ -21,31 +22,33 @@ process {
 
     $Output = @()
     $FoundNewVersion = $false
-    foreach ($Item in $Release) {
+    foreach ($Arch in $Architecture) {
+        foreach ($Rls in $Release) {
 
-        Write-Host "`tInstalling VcRedist $Item." -ForegroundColor "Cyan"
-        Get-VcList -Release $Item | Save-VcRedist -Path $Path | Install-VcRedist -Silent
-        $InstalledVcRedists = Get-InstalledVcRedist | Where-Object { $_.Name -notmatch "Debug Runtime" }
+            Write-Host "`tInstalling VcRedist $Rls." -ForegroundColor "Cyan"
+            Get-VcList -Release $Rls -Architecture $Arch | Save-VcRedist -Path $Path | Install-VcRedist -Silent
+            $InstalledVcRedists = Get-InstalledVcRedist | Where-Object { $_.Name -notmatch "Debug Runtime" }
 
-        # Filter the VcRedists for the target version and compare against what has been installed
-        foreach ($ManifestVcRedist in ($CurrentManifest.Supported | Where-Object { $_.Release -eq $Item })) {
-            $InstalledItem = $InstalledVcRedists | Where-Object { ($_.Release -eq $ManifestVcRedist.Release) -and ($_.Architecture -eq $ManifestVcRedist.Architecture) }
+            # Filter the VcRedists for the target version and compare against what has been installed
+            foreach ($ManifestVcRedist in ($CurrentManifest.Supported | Where-Object { $_.Release -eq $Rls -and $_.Architecture -eq $Arch })) {
+                $InstalledItem = $InstalledVcRedists | Where-Object { ($_.Release -eq $ManifestVcRedist.Release) -and ($_.Architecture -eq $ManifestVcRedist.Architecture) }
 
-            # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
-            if ([System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version) {
-                Write-Host -ForegroundColor "Cyan" "`tVcRedist manifest is out of date."
-                Write-Host -ForegroundColor "Cyan" "`tInstalled version:`t$($InstalledItem.Version)"
-                Write-Host -ForegroundColor "Cyan" "`tManifest version:`t$($ManifestVcRedist.Version)"
+                # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
+                if ([System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version) {
+                    Write-Host -ForegroundColor "Cyan" "`tVcRedist manifest is out of date."
+                    Write-Host -ForegroundColor "Cyan" "`tInstalled version:`t$($InstalledItem.Version)"
+                    Write-Host -ForegroundColor "Cyan" "`tManifest version:`t$($ManifestVcRedist.Version)"
 
-                # Find the index of the VcRedist in the manifest and update it's properties
-                $Index = $CurrentManifest.Supported::IndexOf($CurrentManifest.Supported.ProductCode, $ManifestVcRedist.ProductCode)
-                $CurrentManifest.Supported[$Index].ProductCode = $InstalledItem.ProductCode
-                $CurrentManifest.Supported[$Index].Version = $InstalledItem.Version
+                    # Find the index of the VcRedist in the manifest and update it's properties
+                    $Index = $CurrentManifest.Supported::IndexOf($CurrentManifest.Supported.ProductCode, $ManifestVcRedist.ProductCode)
+                    $CurrentManifest.Supported[$Index].ProductCode = $InstalledItem.ProductCode
+                    $CurrentManifest.Supported[$Index].Version = $InstalledItem.Version
 
-                # Create output variable
-                # $NewVersion = $InstalledItem.Version
-                $FoundNewVersion = $true
-                $Output += $Item
+                    # Create output variable
+                    # $NewVersion = $InstalledItem.Version
+                    $FoundNewVersion = $true
+                    $Output += $Rls
+                }
             }
         }
     }
