def3c03a7c94aa098b721c38ab42dbaa758a009f
Aaron Parker <aaron@stealthpuppy.com>
2025-06-02 09:46:32 +1000

    Wrap Describe blocks

--- tests/Manifest.Tests.ps1
+++ tests/Manifest.Tests.ps1
@@ -25,75 +25,79 @@ BeforeDiscovery {
     }
 }
 
-Describe -Name "VcRedist manifest tests AMD64" -ForEach $ValidateReleasesAmd64 -Skip:$SkipAmd {
-    BeforeAll {
-        Get-InstalledVcRedist | Uninstall-VcRedist -Confirm:$false
-    }
-
-    Context "Validate manifest" {
+Describe -Name "AMD64 specific tests" -Skip:$SkipAmd {
+    Describe -Name "VcRedist manifest tests" -ForEach $ValidateReleasesAmd64 {
         BeforeAll {
-            $VcManifest = "$env:GITHUB_WORKSPACE\VcRedist\VisualCRedistributables.json"
-            Write-Host -ForegroundColor "Cyan" "`tGetting manifest from: $VcManifest."
-            $CurrentManifest = Get-Content -Path $VcManifest | ConvertFrom-Json
-            $VcRedist = $_
-
-            $Path = $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
-            New-Item -Path $Path -ItemType "Directory" -ErrorAction "SilentlyContinue" > $null
-            Save-VcRedist -VcList (Get-VcList -Release $VcRedist) -Path $Path
-
-            $Architectures = @("x86", "x64")
+            Get-InstalledVcRedist | Uninstall-VcRedist -Confirm:$false
         }
 
-        Context "Compare manifest version against installed version for <VcRedist>" -ForEach $Architectures {
-            BeforeEach {
-                $VcList = Get-VcList -Release $VcRedist | Save-VcRedist -Path $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
-                Install-VcRedist -VcList $VcList -Silent
-                $InstalledVcRedists = Get-InstalledVcRedist
+        Context "Validate manifest" {
+            BeforeAll {
+                $VcManifest = "$env:GITHUB_WORKSPACE\VcRedist\VisualCRedistributables.json"
+                Write-Host -ForegroundColor "Cyan" "`tGetting manifest from: $VcManifest."
+                $CurrentManifest = Get-Content -Path $VcManifest | ConvertFrom-Json
+                $VcRedist = $_
 
-                $ManifestVcRedist = $CurrentManifest.Supported | Where-Object { $_.Release -eq $VcRedist }
-                $InstalledItem = $InstalledVcRedists | Where-Object { ($VcRedist -eq $ManifestVcRedist.Release) -and ($_ -eq $ManifestVcRedist.Architecture) }
+                $Path = $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
+                New-Item -Path $Path -ItemType "Directory" -ErrorAction "SilentlyContinue" > $null
+                Save-VcRedist -VcList (Get-VcList -Release $VcRedist) -Path $Path
+
+                $Architectures = @("x86", "x64")
             }
 
-            # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
-            It "$($ManifestVcRedist.Release) $($ManifestVcRedist.Architecture) version should be current" {
-                [System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version | Should -Be $false
+            Context "Compare manifest version against installed version for <VcRedist>" -ForEach $Architectures {
+                BeforeEach {
+                    $VcList = Get-VcList -Release $VcRedist | Save-VcRedist -Path $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
+                    Install-VcRedist -VcList $VcList -Silent
+                    $InstalledVcRedists = Get-InstalledVcRedist
+
+                    $ManifestVcRedist = $CurrentManifest.Supported | Where-Object { $_.Release -eq $VcRedist }
+                    $InstalledItem = $InstalledVcRedists | Where-Object { ($VcRedist -eq $ManifestVcRedist.Release) -and ($_ -eq $ManifestVcRedist.Architecture) }
+                }
+
+                # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
+                It "$($ManifestVcRedist.Release) $($ManifestVcRedist.Architecture) version should be current" {
+                    [System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version | Should -Be $false
+                }
             }
         }
     }
 }
 
-Describe -Name "VcRedist manifest tests ARM64" -ForEach $ValidateReleasesArm64 -Skip:$SkipArm {
-    BeforeAll {
-        Get-InstalledVcRedist | Uninstall-VcRedist -Confirm:$false
-    }
-
-    Context "Validate manifest" {
+Describe -Name "ARM64 specific tests" -Skip:$SkipArm {
+    Describe -Name "VcRedist manifest tests" -ForEach $ValidateReleasesArm64 {
         BeforeAll {
-            $VcManifest = "$env:GITHUB_WORKSPACE\VcRedist\VisualCRedistributables.json"
-            Write-Host -ForegroundColor "Cyan" "`tGetting manifest from: $VcManifest."
-            $CurrentManifest = Get-Content -Path $VcManifest | ConvertFrom-Json
-            $VcRedist = $_
-
-            $Path = $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
-            New-Item -Path $Path -ItemType "Directory" -ErrorAction "SilentlyContinue" > $null
-            Save-VcRedist -VcList (Get-VcList -Release $VcRedist) -Path $Path
-
-            $Architectures = @("arm64")
+            Get-InstalledVcRedist | Uninstall-VcRedist -Confirm:$false
         }
 
-        Context "Compare manifest version against installed version for <VcRedist>" -ForEach $Architectures {
-            BeforeEach {
-                $VcList = Get-VcList -Release $VcRedist | Save-VcRedist -Path $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
-                Install-VcRedist -VcList $VcList -Silent
-                $InstalledVcRedists = Get-InstalledVcRedist
+        Context "Validate manifest" {
+            BeforeAll {
+                $VcManifest = "$env:GITHUB_WORKSPACE\VcRedist\VisualCRedistributables.json"
+                Write-Host -ForegroundColor "Cyan" "`tGetting manifest from: $VcManifest."
+                $CurrentManifest = Get-Content -Path $VcManifest | ConvertFrom-Json
+                $VcRedist = $_
 
-                $ManifestVcRedist = $CurrentManifest.Supported | Where-Object { $_.Release -eq $VcRedist }
-                $InstalledItem = $InstalledVcRedists | Where-Object { ($VcRedist -eq $ManifestVcRedist.Release) -and ($_ -eq $ManifestVcRedist.Architecture) }
+                $Path = $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
+                New-Item -Path $Path -ItemType "Directory" -ErrorAction "SilentlyContinue" > $null
+                Save-VcRedist -VcList (Get-VcList -Release $VcRedist) -Path $Path
+
+                $Architectures = @("arm64")
             }
 
-            # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
-            It "$($ManifestVcRedist.Release) $($ManifestVcRedist.Architecture) version should be current" {
-                [System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version | Should -Be $false
+            Context "Compare manifest version against installed version for <VcRedist>" -ForEach $Architectures {
+                BeforeEach {
+                    $VcList = Get-VcList -Release $VcRedist | Save-VcRedist -Path $([System.IO.Path]::Combine($env:RUNNER_TEMP, "Downloads"))
+                    Install-VcRedist -VcList $VcList -Silent
+                    $InstalledVcRedists = Get-InstalledVcRedist
+
+                    $ManifestVcRedist = $CurrentManifest.Supported | Where-Object { $_.Release -eq $VcRedist }
+                    $InstalledItem = $InstalledVcRedists | Where-Object { ($VcRedist -eq $ManifestVcRedist.Release) -and ($_ -eq $ManifestVcRedist.Architecture) }
+                }
+
+                # If the manifest version of the VcRedist is lower than the installed version, the manifest is out of date
+                It "$($ManifestVcRedist.Release) $($ManifestVcRedist.Architecture) version should be current" {
+                    [System.Version]$InstalledItem.Version -gt [System.Version]$ManifestVcRedist.Version | Should -Be $false
+                }
             }
         }
     }
--- tests/Public/Install-VcRedist.Tests.ps1
+++ tests/Public/Install-VcRedist.Tests.ps1
@@ -26,126 +26,130 @@ BeforeDiscovery {
 	}
 }
 
-Describe -Name "Install-VcRedist with unsupported Redistributables AMD64" -ForEach $UnsupportedReleases {
-	BeforeAll {
-		$Release = $_
+Describe -Name "AMD64 specific tests" -Skip:$SkipAmd {
+	Describe -Name "Install-VcRedist with unsupported Redistributables" -ForEach $UnsupportedReleases {
+		BeforeAll {
+			$Release = $_
 
-		# Create download path
-		if ($env:Temp) {
-			$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
+			# Create download path
+			if ($env:Temp) {
+				$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
+			}
+			elseif ($env:TMPDIR) {
+				$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
+			}
+			elseif ($env:RUNNER_TEMP) {
+				$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
+			}
+			New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $null
 		}
-		elseif ($env:TMPDIR) {
-			$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
+
+		Context "Install <Release[0].Name> Redistributable" {
+			BeforeAll {
+				$VcRedist = $Release | Save-VcRedist -Path $Path
+			}
+
+			It "Installs OK via parameters" {
+				{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
+			}
 		}
-		elseif ($env:RUNNER_TEMP) {
-			$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
-		}
-		New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $null
 	}
 
-	Context "Install <Release[0].Name> Redistributable" -Skip:$SkipAmd {
+	Describe -Name "Install-VcRedist with supported Redistributables" -ForEach $SupportedReleasesAmd64 {
 		BeforeAll {
-			$VcRedist = $Release | Save-VcRedist -Path $Path
+			$Release = $_
+
+			# Create download path
+			if ($env:Temp) {
+				$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
+			}
+			elseif ($env:TMPDIR) {
+				$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
+			}
+			elseif ($env:RUNNER_TEMP) {
+				$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
+			}
+			New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $Null
 		}
 
-		It "Installs OK via parameters" {
-			{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
+		Context "Install <Release> x64 Redistributable" {
+			BeforeAll {
+				$VcRedist = Get-VcList -Release $Release -Architecture "x64" | Save-VcRedist -Path $Path
+			}
+
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
+				{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
+			}
+
+			It "Returns the list of installed VcRedists after install" {
+				Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
+			}
+
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
+				{ Get-VcList -Release $Release -Architecture "x64" | `
+						Save-VcRedist -Path $Path | `
+						Install-VcRedist -Silent } | Should -Not -Throw
+			}
+		}
+
+		Context "Install <Release> x86 Redistributable" {
+			BeforeAll {
+				$VcRedist = Get-VcList -Release $Release -Architecture "x86" | Save-VcRedist -Path $Path
+			}
+
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
+				{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
+			}
+
+			It "Returns the list of installed VcRedists after install" {
+				Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
+			}
+
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
+				{ Get-VcList -Release $Release -Architecture "x86" | `
+						Save-VcRedist -Path $Path | `
+						Install-VcRedist -Silent } | Should -Not -Throw
+			}
 		}
 	}
 }
 
-Describe -Name "Install-VcRedist with supported Redistributables AMD64" -ForEach $SupportedReleasesAmd64 -Skip:$SkipAmd {
-	BeforeAll {
-		$Release = $_
-
-		# Create download path
-		if ($env:Temp) {
-			$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
-		}
-		elseif ($env:TMPDIR) {
-			$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
-		}
-		elseif ($env:RUNNER_TEMP) {
-			$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
-		}
-		New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $Null
-	}
-
-	Context "Install <Release> x64 Redistributable" {
+Describe -Name "ARM64 specific tests" -Skip:$SkipArm {
+	Describe -Name "Install-VcRedist with supported Redistributables" -ForEach $SupportedReleasesArm64 {
 		BeforeAll {
-			$VcRedist = Get-VcList -Release $Release -Architecture "x64" | Save-VcRedist -Path $Path
+			$Release = $_
+
+			# Create download path
+			if ($env:Temp) {
+				$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
+			}
+			elseif ($env:TMPDIR) {
+				$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
+			}
+			elseif ($env:RUNNER_TEMP) {
+				$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
+			}
+			New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $Null
 		}
 
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
-			{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
-		}
+		Context "Install <Release> arm64 Redistributable" {
+			BeforeAll {
+				$VcRedist = Get-VcList -Release $Release -Architecture "arm64" | Save-VcRedist -Path $Path
+			}
 
-		It "Returns the list of installed VcRedists after install" {
-			Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
-		}
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
+				{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
+			}
 
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
-			{ Get-VcList -Release $Release -Architecture "x64" | `
-					Save-VcRedist -Path $Path | `
-					Install-VcRedist -Silent } | Should -Not -Throw
-		}
-	}
+			It "Returns the list of installed VcRedists after install" {
+				Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
+			}
 
-	Context "Install <Release> x86 Redistributable" {
-		BeforeAll {
-			$VcRedist = Get-VcList -Release $Release -Architecture "x86" | Save-VcRedist -Path $Path
-		}
-
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
-			{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
-		}
-
-		It "Returns the list of installed VcRedists after install" {
-			Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
-		}
-
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
-			{ Get-VcList -Release $Release -Architecture "x86" | `
-					Save-VcRedist -Path $Path | `
-					Install-VcRedist -Silent } | Should -Not -Throw
-		}
-	}
-}
-
-Describe -Name "Install-VcRedist with supported Redistributables ARM64" -ForEach $SupportedReleasesArm64 -Skip:$SkipArm {
-	BeforeAll {
-		$Release = $_
-
-		# Create download path
-		if ($env:Temp) {
-			$Path = Join-Path -Path $env:Temp -ChildPath "Downloads"
-		}
-		elseif ($env:TMPDIR) {
-			$Path = Join-Path -Path $env:TMPDIR -ChildPath "Downloads"
-		}
-		elseif ($env:RUNNER_TEMP) {
-			$Path = Join-Path -Path $env:RUNNER_TEMP -ChildPath "Downloads"
-		}
-		New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $Null
-	}
-
-	Context "Install <Release> arm64 Redistributable" {
-		BeforeAll {
-			$VcRedist = Get-VcList -Release $Release -Architecture "arm64" | Save-VcRedist -Path $Path
-		}
-
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via parameters" {
-			{ Install-VcRedist -VcList $VcRedist -Silent } | Should -Not -Throw
-		}
-
-		It "Returns the list of installed VcRedists after install" {
-			Install-VcRedist -VcList $VcRedist -Silent | Should -BeOfType "System.Management.Automation.PSObject"
-		}
-
-		It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
-			{ Get-VcList -Release $Release -Architecture "arm64" | `
-					Save-VcRedist -Path $Path | `
-					Install-VcRedist -Silent } | Should -Not -Throw
+			It "Installs the VcRedist: <VcRedist.Name> <VcRedist.Architecture> via the pipeline" {
+				{ Get-VcList -Release $Release -Architecture "arm64" | `
+						Save-VcRedist -Path $Path | `
+						Install-VcRedist -Silent } | Should -Not -Throw
+			}
 		}
 	}
 }
