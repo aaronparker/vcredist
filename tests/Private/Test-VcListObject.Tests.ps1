d34cdb98a53c8f1ba6520653a270f4468364db2d
Aaron Parker <aaron@stealthpuppy.com>
2025-06-29 11:19:08 +1000

    Additional Pester tests

--- tests/Private/Test-VcListObject.Tests.ps1
+++ tests/Private/Test-VcListObject.Tests.ps1
@@ -26,6 +26,7 @@ InModuleScope VcRedist {
 			UninstallKey    = "32"
 			Path            = "C:\Temp\VcRedist.exe"
 			PackageId       = "b1e3c2a7-8f2d-4c3a-9e2a-7c4b1e2d3f4a"
+			DetectionFile   = "%SystemRoot%\\System32\\vcruntime140.dll"
 		}
 
 		$InvalidObject = [PSCustomObject]@{
