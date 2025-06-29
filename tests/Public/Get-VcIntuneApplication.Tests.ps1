102f3a533736d4ca5260723b88aead3be1a7ce69
Aaron Parker <aaron@stealthpuppy.com>
2025-06-29 18:52:22 +1000

    Update Get-VcIntuneApplication.Tests.ps1

--- tests/Public/Get-VcIntuneApplication.Tests.ps1
+++ tests/Public/Get-VcIntuneApplication.Tests.ps1
@@ -7,6 +7,24 @@ param ()
 
 Describe "Get-VcIntuneApplication" {
     BeforeAll {
+        foreach ($Module in @("MSAL.PS", "IntuneWin32App")) {
+            Install-Module -Name $Module -Force
+        }
+
+        try {
+            # Authenticate to the Graph API
+            # Expects secrets to be passed into environment variables
+            Write-Information -MessageData "Authenticate to the Graph API"
+            $params = @{
+                TenantId     = "$env:TENANT_ID"
+                ClientId     = "$env:CLIENT_ID"
+                ClientSecret = "$env:CLIENT_SECRET"
+            }
+            $script:AuthToken = Connect-MSIntuneGraph @params
+        }
+        catch {
+            throw $_
+        }
     }
 
     It "Should not throw when called" {
